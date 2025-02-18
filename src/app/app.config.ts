89bc725361afc0a1a2ec07fc756faa22cf5c4c12
Marina Kotova <m.klementyeva@mail.ru>
2025-02-18 11:09:46 +0700

    Auto-generated commit

--- src/app/app.config.ts
+++
@@ -1,8 +0,0 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { provideClientHydration } from '@angular/platform-browser';
-import { routes } from './todos/todos.module';
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
-};
--- src/app/todos/components/todos/todos.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-todos',
-  templateUrl: './todos.component.html',
-})
-export class TodosComponent {
-
-}
